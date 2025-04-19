2ed61ec13b59c254c3df1c395e5ae7ba434897bd
Anshrin Srivastava <Anshrin@gmail.com>
2025-04-19 00:41:47 -0400

    Update barriers.jl

--- src/barriers.jl
+++ src/barriers.jl
@@ -9,24 +9,28 @@ end
 cardinals() = (Vec2(1,0), Vec2(0,1), Vec2(-1,0), Vec2(0,-1))
 
 function barrier_stop(b::CardinalBarriers, from, delta)
-    # https://stackoverflow.com/questions/563198/whats-the-most-efficent-way-to-calculate-where-two-line-segments-intersect
-    shortest_u = 1.0+2.0*eps()
+    shortest_u = 1.0 + 2.0 * eps()
     q = from
     s = delta
     for dir in cardinals()
-        p = b.start*dir
-        r = b.len*dir
-        rxs = cross(r,s)
-        if rxs == 0.0 # parallel (free pass - this won't force a stop)
+        p = b.start * dir
+        r = b.len * dir
+        rxs = cross(r, s)
+        if isapprox(rxs, 0.0; atol=1e-8)
             continue
         else
-            qmp = q-p
-            u = cross(qmp, r)/rxs
-            t = cross(qmp, s)/rxs
+            qmp = q - p
+            u = cross(qmp, r) / rxs
+            t = cross(qmp, s) / rxs
             if 0.0 <= u < shortest_u && 0.0 <= t <= 1.0
                 shortest_u = u
             end
         end
     end
-    return from + (shortest_u-2.0*eps())*delta
+    result = from + (shortest_u - 2.0 * eps()) * delta
+    if any(isnan, result)
+        @warn "NaN in barrier_stop result. from=$from delta=$delta"
+        return from
+    end
+    return result
 end
