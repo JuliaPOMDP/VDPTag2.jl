3abb8b9ee7e276aa6616b3496254cb55c0db11a4
Anshrin Srivastava <Anshrin@gmail.com>
2025-04-18 23:58:45 -0400

    Update VDPTag2.jl

--- src/VDPTag2.jl
+++ src/VDPTag2.jl
@@ -97,11 +97,16 @@ next_ml_target(p::VDPTagMDP, pos::AbstractVector) = next_ml_target(p, convert(Ve
 function POMDPs.transition(pp::VDPTagProblem, s::TagState, a::Float64)
     ImplicitDistribution(pp, s, a) do pp, s, a, rng
         p = mdp(pp)
-        targ = next_ml_target(p, s.target) + p.pos_std*SVector(randn(rng), randn(rng))
-        agent = barrier_stop(p.barriers, s.agent, p.agent_speed*p.step_size*SVector(cos(a), sin(a)))
+        targ = next_ml_target(p, s.target) + p.pos_std * SVector(randn(rng), randn(rng))
+        agent = barrier_stop(p.barriers, s.agent, p.agent_speed * p.step_size * SVector(cos(a), sin(a)))
+
+        if any(isnan, targ) || any(isnan, agent)
+            error("Generated NaN in transition! agent=$agent, target=$targ")
+        end
         return TagState(agent, targ)
     end
 end
+
 
 function POMDPs.reward(pp::VDPTagProblem, s::TagState, a::Float64, sp::TagState)
     p = mdp(pp)
