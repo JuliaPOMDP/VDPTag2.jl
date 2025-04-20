9b8a14630714078eaa1781f6c58e7ede6702c768
Anshrin Srivastava <Anshrin@gmail.com>
2025-04-20 18:37:14 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+import VDPTag2: convert_s, convert_a, convert_o
 using Test
 using VDPTag2
 using POMDPs
@@ -5,27 +6,27 @@ using Random
 using ParticleFilters
 using Distributions
 
-# Use a consistent random seed
+# Set up consistent randomness
 rng = MersenneTwister(123)
 
 @testset "Discrete State, Action, and Observation Conversion" begin
     dp = AODiscreteVDPTagPOMDP()
 
-    # Create a simple state and action
+    # Example state and action
     s = TagState(Vec2(0.2, 0.3), Vec2(-0.5, -0.4))
     a = TagAction(true, π/4)
 
-    # Test state conversion to integer and back
+    # State: convert to discrete and back
     s_int = convert_s(Int, s, dp)
     s_back = convert_s(TagState, s_int, dp)
     @test s_back isa TagState
 
-    # Test action conversion to integer and back
+    # Action: convert to discrete and back
     a_int = convert_a(Int, a, dp)
     a_back = convert_a(TagAction, a_int, dp)
     @test a_back isa TagAction
 
-    # Test observation conversion
+    # Observation: generate and convert to discrete
     obs_vec = rand(rng, observation(cproblem(dp), s, a, s))
     obs_disc = convert_o(IVec8, obs_vec, dp)
     @test obs_disc isa IVec8
@@ -35,15 +36,15 @@ end
     dp1 = AODiscreteVDPTagPOMDP()
     dp2 = ADiscreteVDPTagPOMDP()
     s = TagState(Vec2(0.0, 0.0), Vec2(1.0, 1.0))
-    a = 1  # integer action
+    a = 1  # Discrete action index
 
-    # Run gen function and check types
+    # Generate next state and observation
     r1 = POMDPs.gen(dp1, s, a, rng)
     r2 = POMDPs.gen(dp2, s, a, rng)
     @test r1.sp isa TagState
     @test r2.sp isa TagState
 
-    # Run observation function
+    # Observation function
     o1 = rand(rng, POMDPs.observation(dp1, s, a, s))
     o2 = POMDPs.observation(dp2, s, a, s)
     @test o1 isa IVec8
@@ -54,20 +55,21 @@ end
     pomdp = VDPTagPOMDP()
     mdp_model = mdp(pomdp)
 
-    # ToNextML policy chooses direction based on current state
+    # ToNextML policy: angle-only control
     policy = ToNextML(mdp_model; rng=rng)
     s = TagState(Vec2(0.0, 0.0), Vec2(1.0, 1.0))
     angle = POMDPs.action(policy, s)
     @test angle isa Float64
 
-    # ManageUncertainty policy adds 'look' based on uncertainty
+    # ManageUncertainty: determines 'look' from particle spread
     states = [TagState(Vec2(0.0, 0.0), Vec2(1.0, 1.0 + 0.01*i)) for i in 1:50]
     belief = ParticleCollection(states)
     policy2 = ManageUncertainty(pomdp, 0.01)
     a2 = POMDPs.action(policy2, belief)
     @test a2 isa TagAction
+    @test typeof(a2.look) == Bool
 
-    # TranslatedPolicy maps actions from one model space to another
+    # TranslatedPolicy: maps from one model’s state space to another’s
     dp = ADiscreteVDPTagPOMDP()
     translated = translate_policy(policy, mdp_model, dp, dp)
     a_translated = POMDPs.action(translated, s)
