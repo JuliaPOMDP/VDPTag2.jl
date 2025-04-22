7069bd9acbffbd2c728602100eba3fab079129ea
Anshrin Srivastava <Anshrin@gmail.com>
2025-04-22 01:21:45 -0400

    Update visualization_tests.jl

--- test/visualization_tests.jl
+++ test/visualization_tests.jl
@@ -4,36 +4,33 @@ using POMDPTools
 using VDPTag2: VDPTagProblem, VDPTagPOMDP, TagState, ParticleCollection, mdp, Vec2
 
 @testset "Visualization Recipes" begin
-    pomdp = VDPTagPOMDP()
+    # Define pomdp and mdp with visible barriers
+    pomdp = VDPTagPOMDP(mdp=VDPTagMDP(barriers=CardinalBarriers(0.0, 2.0)))
     p = mdp(pomdp)
 
-    # Plot the VDPTagProblem (covers: @recipe function f(p::VDPTagProblem))
     @test begin
         plt = plot(p)
         !isempty(plt.series_list)
     end
 
-    # Create a dummy history
+    # Simulate with updater
     sim = RolloutSimulator()
-    hist = simulate(sim, pomdp, RandomPolicy(pomdp), 3)
+    updater = BootstrapFilter(pomdp, 10)
+    hist = simulate(sim, pomdp, RandomPolicy(pomdp), updater, 3)
 
-    # Plot with POMDP and history (covers: @recipe function f(pomdp::VDPTagPOMDP, h::...))
     @test begin
         plt = plot(pomdp, hist)
         !isempty(plt.series_list)
     end
 
-    # Plot with problem and history (covers: @recipe function f(p::VDPTagProblem, h::...))
     @test begin
         plt = plot(p, hist)
         !isempty(plt.series_list)
     end
 
-    # Plot particle collection (covers: @recipe function f(pc::ParticleCollection{TagState}))
     @test begin
         pc = ParticleCollection([TagState([1.0, 2.0], [3.0, 4.0]) for _ in 1:10])
         plt = plot(pc)
         !isempty(plt.series_list)
     end
 end
-
