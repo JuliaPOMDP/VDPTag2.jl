eead7fcbd36d77afb90ce6006bf152ea93f01d99
Anshrin Srivastava <Anshrin@gmail.com>
2025-04-22 00:29:17 -0400

    Create visulization_tests.jl

---
+++ test/visulization_tests.jl
@@ -0,0 +1,40 @@
+using VDPTag2
+using Test
+using POMDPTools
+using Plots
+
+@testset "Visualization Recipes" begin
+    # Create a dummy VDPTagProblem
+    m = VDPTagMDP()
+    p = VDPTagProblem(m)
+
+    # Plot the VDPTagProblem (covers: @recipe function f(p::VDPTagProblem))
+    @test begin
+        plt = plot(p)
+        !isempty(plt.series_list)
+    end
+
+    # Create a dummy history
+    pomdp = VDPTagPOMDP(m)
+    sim = RolloutSimulator()
+    hist = simulate(sim, pomdp, RandomPolicy(pomdp), 3)
+
+    # Plot with POMDP and history (covers: @recipe function f(pomdp::VDPTagPOMDP, h::...))
+    @test begin
+        plt = plot(pomdp, hist)
+        !isempty(plt.series_list)
+    end
+
+    # Plot with problem and history (covers: @recipe function f(p::VDPTagProblem, h::...))
+    @test begin
+        plt = plot(p, hist)
+        !isempty(plt.series_list)
+    end
+
+    # Plot particle collection (covers: @recipe function f(pc::ParticleCollection{TagState}))
+    @test begin
+        pc = ParticleCollection([TagState([1.0, 2.0], [3.0, 4.0]) for _ in 1:10])
+        plt = plot(pc)
+        !isempty(plt.series_list)
+    end
+end
